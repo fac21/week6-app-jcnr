2b01ee0dae561751f7bff7656ef7a52062fa0dde
Craig MacLeod <c92macleod@gmail.com>
2021-04-28 12:51:00 +0100

    initilased files
    Co-authored-by: Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
        Co-authored-by: Rosie O'Donnell <61861015+Rosie-ODonnell@users.noreply.github.com>
        Co-authored-by: ByteSized <60395899+ByteSizedIT@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const server = express();
+
+const bodyParser = express.urlencoded()
+
+function logger(req, res, next) {
+    console.log(req.method + " " + req.url);
+    next();
+}
+
+server.use(logger)
+
+
+
+server.get("/", (req, res) => {
+    res.send("<h1>OUR TEAM ROCKS</h1>")
+})
+
+// server.post("/submit", bodyParser, (req, res) => {
+    
+// })
+
+server.use((req, res) => {
+    res.status(404).send("<h1>Not found</h1>")
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`started on http://localhost:${PORT}`))
