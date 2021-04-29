450c08d554d289efd6f76c4ace5c1de263575f17
Craig MacLeod <c92macleod@gmail.com>
2021-04-29 11:45:44 +0100

    template for homepage
    Co-authored-by: Rosie O'Donnell <61861015+Rosie-ODonnell@users.noreply.github.com>

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,11 +1,21 @@
 const db = require("../database/connection.js");
 const model = require("../database/model.js");
+const html = require("../templates.js")
+
+
 
 function get(request, response) {
-  console.log(`entered get function in homepage.js`);
-  model.viewAllUserData().then(() => {
-    response.send("<h1>Hello world</h1>");
-  });
+  db.query("SELECT reviews.park_name, reviews.park_location, reviews.review_content, users.username FROM reviews INNER JOIN users ON reviews.user_id = users.id")
+  .then((result) => {
+    console.log(result);
+    const reviews = result.rows; // an array of obecjts, where each object is a row from database (parkname, review content etc)
+    const reviewList = reviews.map((review) => {
+      return`
+      <h1>${review.park_name}</h1>
+      `
+    }).join("")
+    response.send(html.htmlTemplate(reviewList))
+  })
 }
 
 module.exports = { get };
---
+++ templates.js
@@ -0,0 +1,27 @@
+function htmlTemplate(content) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="">
+      <link rel="icon" type="image/png" href="#"/>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+      <title>Grinder</title>
+    </head>
+    <body>
+    <header>
+    /* <img src="/images/facbook-logo.png" alt="facbook logo"><br/> */
+    <a href="/ROUTE FOR LOGIN">Login / Sign Up</a>
+    </header>
+        <main>
+      ${content}
+      </main>
+    </body>
+    </html>
+    `;
+};
+
+module.exports = { htmlTemplate };
