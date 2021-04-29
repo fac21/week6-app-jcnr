662c27e0f2e92120c4d68908e26e1a731c0d4d65
ByteSized <neville.keemer@gmail.com>
2021-04-29 17:30:03 +0100

    Debugged add review route.
    Co-authored-by: Jo Humphrey <31373245+jamdelion@users.noreply.github.com>

--- routes/postReview.js
+++ routes/postReview.js
@@ -2,18 +2,22 @@ const db = require("../database/connection.js");
 const model = require("../database/model.js");
 
 function post(request, response) {
-  //console.log(request.body);
+  console.log(request.body);
   const { parkname, location, review } = request.body;
   const sid = request.signedCookies.sid;
-  console.log({ sid });
+  //console.log({ sid });
   model
     .getSession(sid)
-    .then((session) => {
-      const username = session.user.username;
-      return model.getUser(username); ///returning whole user
-    })
-    .then((userID) => {
-      model.createReview(parkname, location, review, userID);
+    .then(
+      (session) =>
+        //console.log({ session });
+        //const email = session.user.email;
+        //console.log({ email });
+        model.getUser(session.user.email) ///returning whole user
+    )
+    .then((user) => {
+      //console.log(parkname, location, review, user.id);
+      model.createReview(parkname, location, review, user.id);
       //console.log(parkname, location, review, userID);
     })
     .then(() => {
